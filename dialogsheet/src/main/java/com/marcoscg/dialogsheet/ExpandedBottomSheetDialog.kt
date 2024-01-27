14eb3462ea0f5a57b4c5e094de446be4470e8fb4
Marcos Calvo García <marcoscgdev@gmail.com>
2024-01-27 21:07:43 +0100

    Dialog landscape width was parameterized

--- dialogsheet/src/main/java/com/marcoscg/dialogsheet/ExpandedBottomSheetDialog.kt
+++ dialogsheet/src/main/java/com/marcoscg/dialogsheet/ExpandedBottomSheetDialog.kt
@@ -1,35 +1,16 @@
 package com.marcoscg.dialogsheet
 
 import android.content.Context
-import android.os.Bundle
-import android.widget.FrameLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
-internal class ExpandedBottomSheetDialog(context: Context, theme: Int) : BottomSheetDialog(context, theme) {
-
-    private var dialogBehavior: BottomSheetBehavior<FrameLayout>? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        try {
-            val privateField = BottomSheetDialog::class.java.getDeclaredField("behavior")
-            privateField.isAccessible = true
-            dialogBehavior = privateField[this] as BottomSheetBehavior<FrameLayout>
-        } catch (e: NoSuchFieldException) {
-            // do nothing
-        } catch (e: IllegalAccessException) {
-            // do nothing
-        }
-    }
+internal class ExpandedBottomSheetDialog(context: Context, theme: Int) :
+    BottomSheetDialog(context, theme) {
 
     override fun onStart() {
         super.onStart()
 
-        if (dialogBehavior != null) {
-            dialogBehavior!!.skipCollapsed = true
-            dialogBehavior!!.state = BottomSheetBehavior.STATE_EXPANDED
-        }
+        behavior.skipCollapsed = true
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
     }
 }
